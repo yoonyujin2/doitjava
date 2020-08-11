737986515de5bc6ffc014701b21a385647e6e801
yn-13 <yn-13@LAPTOP-4CA3NVJT>
2020-08-11 14:55:34 +0900

    자바 1일차 예제

---
+++ doitjava/src/doitjava/Gittest.java
@@ -0,0 +1,10 @@
+package doitjava;
+
+public class Gittest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("깃허브 테스트");
+	}
+
+}
---
+++ doitjava/src/doitjava/Hello.java
@@ -0,0 +1,10 @@
+package doitjava;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+	}
+
+}
