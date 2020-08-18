43574ecfaa2498bdaf5220bb541c048b8dc5c7b3
YoonYuJin <yoonyujin.dev@gmail.com>
2020-08-18 15:30:41 +0900

    OperationEx4

---
+++ doitjava/src/chapter03/OperationEx4.java
@@ -0,0 +1,26 @@
+package chapter03;
+
+public class OperationEx4 {
+
+	public static void main(String[] args) {
+		int fatherAge = 45;
+		int motherAge = 47;
+		
+		char ch;
+		ch = (fatherAge > motherAge) ? 'T' : 'F';
+		
+		System.out.println(ch);
+		
+		int num = 10;
+		boolean isEven;
+		isEven = (num % 2) == 0 ? true : false;
+		System.out.println("礎熱檣陛蹂? " + (isEven ? "礎熱" : "�汝�")公);
+		
+		
+		
+		
+				
+
+	}
+
+}
